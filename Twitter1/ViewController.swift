f361fd13885b697f7da4c08a7000a22d63cf3561
zhany <zhanybeksalgarin@gmail.com>
2022-05-11 08:15:45 -0400

    Initial Commit

---
+++ Twitter1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter1
+//
+//  Created by zhanybek salgarin on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
