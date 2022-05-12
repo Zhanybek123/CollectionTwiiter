6ae9723973b888d5db1d729a29f2e3647e3257d3
zhany <zhanybeksalgarin@gmail.com>
2022-05-12 14:31:32 -0400

    First commit

---
+++ MainView/CollectionViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  MainView.swift
+//  Twitter1
+//
+//  Created by zhanybek salgarin on 5/12/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    let cellId = "cellId"
+     
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test Test Test"
+        label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemRed
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+//    this gets called when a cell is dequeued
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        
+    }
+    
+    func setUpViews() {
+        backgroundColor = .yellow
+        addSubview(wordLabel)
+        addSubview(profileImageView)
+        NSLayoutConstraint.activate([
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor),
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor),
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor),
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        
+        profileImageView.topAnchor.constraint(equalTo: wordLabel.topAnchor, constant: 16),
+        profileImageView.leftAnchor.constraint(equalTo: wordLabel.leftAnchor,constant: 16),
+        profileImageView.widthAnchor.constraint(equalToConstant: 50),
+        profileImageView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Twitter1/AppDelegate.swift
+++ Twitter1/AppDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+//         Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+//        let homecontroller = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: HomeController())
+        window?.makeKeyAndVisible()
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
